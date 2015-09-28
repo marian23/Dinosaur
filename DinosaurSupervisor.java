fd3ebb99811517a706cf9d4b903ee711e66fd1b4
marian <marian@email.com>
2015-09-28 02:46:32 -0700

    comment first

---
+++ DinosaurSupervisor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by marian on 9/28/2015.
+ */
+public class DinosaurSupervisor {
+    public static void main(String[] args){
+        Dinosaurs t_rex = new Dinosaurs();
+        Dinosaurs velociraptor = new Dinosaurs();
+
+        t_rex.roar();
+        velociraptor.roar();
+
+    }
+}
---
+++ Dinosaurs.java
@@ -0,0 +1,11 @@
+/**
+ * Created by marian on 9/28/2015.
+ */
+public class Dinosaurs {
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+
+
+
+    }
+}
